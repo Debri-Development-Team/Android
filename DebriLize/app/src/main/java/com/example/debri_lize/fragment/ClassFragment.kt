e2fcd6fdb672e518e29cd00f105a25dddc0e95c9
slr-09 <ge12090@naver.com>
2022-07-25 15:18:19 +0900

    layout

--- DebriLize/app/src/main/java/com/example/debri_lize/fragment/ClassFragment.kt
+++ DebriLize/app/src/main/java/com/example/debri_lize/fragment/ClassFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.debri_lize.BoardFavoriteRVAdapter
 import com.example.debri_lize.ClassFavoriteRVAdapter
+import com.example.debri_lize.R
 import com.example.debri_lize.data.Lecture
 import com.example.debri_lize.databinding.FragmentClassBinding
 
@@ -32,6 +33,103 @@ class ClassFragment : Fragment() {
         super.onStart()
 
         initClassFavoriteRecycler()
+        onRadioButtonClicked()
+
+    }
+
+    private fun onRadioButtonClicked(){
+        //언어
+        //front
+        binding.classCurriTagFrontCb.setOnCheckedChangeListener { button, checked ->
+            if(checked){
+                binding.classCurriTagBackCb.visibility = View.GONE
+                binding.classCurriTagCCb.visibility = View.GONE
+                binding.classCurriTagPythonCb.visibility = View.GONE
+            }else{
+                binding.classCurriTagBackCb.visibility = View.VISIBLE
+                binding.classCurriTagCCb.visibility = View.VISIBLE
+                binding.classCurriTagPythonCb.visibility = View.VISIBLE
+            }
+        }
+        //back
+        binding.classCurriTagBackCb.setOnCheckedChangeListener { button, checked ->
+            if(checked){
+                binding.classCurriTagFrontCb.visibility = View.GONE
+                binding.classCurriTagCCb.visibility = View.GONE
+                binding.classCurriTagPythonCb.visibility = View.GONE
+            }else{
+                binding.classCurriTagFrontCb.visibility = View.VISIBLE
+                binding.classCurriTagCCb.visibility = View.VISIBLE
+                binding.classCurriTagPythonCb.visibility = View.VISIBLE
+            }
+        }
+        //C language
+        binding.classCurriTagCCb.setOnCheckedChangeListener { button, checked ->
+            if(checked){
+                binding.classCurriTagBackCb.visibility = View.GONE
+                binding.classCurriTagFrontCb.visibility = View.GONE
+                binding.classCurriTagPythonCb.visibility = View.GONE
+            }else{
+                binding.classCurriTagBackCb.visibility = View.VISIBLE
+                binding.classCurriTagFrontCb.visibility = View.VISIBLE
+                binding.classCurriTagPythonCb.visibility = View.VISIBLE
+            }
+        }
+        //python
+        binding.classCurriTagPythonCb.setOnCheckedChangeListener { button, checked ->
+            if(checked){
+                binding.classCurriTagBackCb.visibility = View.GONE
+                binding.classCurriTagCCb.visibility = View.GONE
+                binding.classCurriTagFrontCb.visibility = View.GONE
+            }else{
+                binding.classCurriTagBackCb.visibility = View.VISIBLE
+                binding.classCurriTagCCb.visibility = View.VISIBLE
+                binding.classCurriTagFrontCb.visibility = View.VISIBLE
+            }
+        }
+
+        //서적 or 영상
+        //서적
+        binding.classCurriTagBookCb.setOnCheckedChangeListener { button, checked ->
+            if(checked){
+                binding.classCurriTagVideoCb.visibility = View.GONE
+            }else{
+                binding.classCurriTagVideoCb.visibility = View.VISIBLE
+            }
+        }
+        //영상
+        binding.classCurriTagVideoCb.setOnCheckedChangeListener { button, checked ->
+            if(checked){
+                binding.classCurriTagBookCb.visibility = View.GONE
+            }else{
+                binding.classCurriTagBookCb.visibility = View.VISIBLE
+            }
+        }
+
+        //가격
+        //무료
+        binding.classPriceTagForfreeCb.setOnCheckedChangeListener { button, checked ->
+            if(checked){
+                binding.classPriceTagNoForfreeCb.visibility = View.GONE
+            }else{
+                binding.classPriceTagNoForfreeCb.visibility = View.VISIBLE
+            }
+        }
+        //유료
+        binding.classPriceTagNoForfreeCb.setOnCheckedChangeListener { button, checked ->
+            if(checked){
+                binding.classPriceTagForfreeCb.visibility = View.GONE
+            }else{
+                binding.classPriceTagForfreeCb.visibility = View.VISIBLE
+            }
+        }
+
+    }
+
+    private fun closeBtnClick(view:View){
+        view.setOnClickListener{
+
+        }
     }
 
     private fun initClassFavoriteRecycler() {
